062ee9c3f129a1022ac7badc312133c61d6d95c7
Chris Bick <chris@apptop.com>
2016-05-10 09:56:50 -0400

    Initial Commit

---
+++ St. Pius X School App/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  St. Pius X School App
+//
+//  Created by Chris Bick on 5/10/16.
+//  Copyright © 2016 St. Pius X School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

05ef85acc6453e20de5f312bd7c1b99897fd3bb2
Chris Bick <appledev@stpius-x.com>
2016-05-11 22:30:22 -0400

    Revert "Revert "Initial Commit""
    This reverts commit 3824f25c4a72ee887a49b1b036f9bc64fd5cbfec.

---
+++ St. Pius X School App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  St. Pius X School App
+//
+//  Created by Chris Bick on 5/10/16.
+//  Copyright © 2016 St. Pius X School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ St. Pius X School App/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  St. Pius X School App
+//
+//  Created by Chris Bick on 5/10/16.
+//  Copyright © 2016 St. Pius X School. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
